ee4d61399d37dd27cd9044e80ad133026f0185b8
skflowne <1980370+skflowne@users.noreply.github.com>
2024-06-12 13:32:47 +0200

    Rollup build workflow

---
+++ example/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+
+import DummyComponent from "react-dsi18n"
+
+const App = props => {
+    return (
+        <div>
+            <h1>Example</h1>
+            <DummyComponent />
+        </div>
+    )
+}
+
+export default App
